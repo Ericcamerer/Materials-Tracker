8269efb024cecb777967b7ee7c2e5bab352e0d29
Eric Camerer <ericcamerer@gmail.com>
2023-07-29 11:40:04 -0400

    Adding VUE Elements

---
+++ MaterialsTracker/MT VUE/src/components/DepositForm.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <label for="full-name">Full Name:</label>
+    <input type="text" id="full-name" v-model="fullName" />
+
+    <label for="date">Date:</label>
+    <input type="date" id="date" v-model="date" />
+
+    <label for="donor">Donor:</label>
+    <input type="text" id="donor" v-model="donor" />
+
+    <label for="material-type">Material Type:</label>
+    <select id="material-type" v-model="materialType">
+      <option value="wood">Wood</option>
+      <option value="glass">Glass</option>
+      <option value="hardware">Hardware</option>
+    </select>
+
+    <!-- You can add a button or any other functionality as needed -->
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      fullName: '',
+      date: '',
+      donor: '',
+      materialType: 'wood', // Default value, change as needed
+    };
+  },
+};
+</script>
